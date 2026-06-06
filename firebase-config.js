41b5f779f5daca316db227792eab6c5cbfcef482
Hogamboy <hogam456@gmail.com>
2026-06-07 03:28:25 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+window.BUSTERCALL_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDJoUJEY9-IRtEHi3xcNMH772R-_dEBJRM",
+  authDomain: "bustercall-cd141.firebaseapp.com",
+  databaseURL: "https://bustercall-cd141-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "bustercall-cd141",
+  storageBucket: "bustercall-cd141.firebasestorage.app",
+  messagingSenderId: "219351466395",
+  appId: "1:219351466395:web:3fdab70f2868e15f86e13e",
+  measurementId: "G-5W8B7FZ74G"
+};
